cac00ae2337a136c063c1a72a52a55114d595375
Andrew Morton <ahjmorton@gmail.com>
2013-12-24 14:06:31 +0000

    Added empty implementation of bmpLib

---
+++ src/bmpLib/bmpLib.c
@@ -0,0 +1,9 @@
+#include <stddef.h>
+
+#include "bmpLib.h"
+
+static bmp_result ERROR = {.result = NULL, .resultSize = -1};
+
+bmp_result create_bw_bmp(const unsigned char * data, const unsigned int dataLen) {
+    return ERROR;   
+}
---
+++ src/bmpLib/bmpLib.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_BMP_LIB
+#define HEADER_BMP_LIB
+
+struct _bmp_result_holder {
+    int resultSize;
+    unsigned char * result;
+};
+
+typedef struct _bmp_result_holder bmp_result; 
+
+bmp_result create_bw_bmp(const unsigned char * data, const unsigned int dataLen);
+
+#endif
