c6b6f933d2bb69834cb494ecef23b4ce33023a8e
Andrew Morton <ahjmorton@gmail.com>
2013-12-27 22:47:27 +0000

    Added calculation of pixel sizings for DIB header

--- src/bmpLib/bmpLib.c
+++ src/bmpLib/bmpLib.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <limits.h>
 
 #include "bmpLib.h"
 
@@ -9,20 +10,55 @@ static const uint32_t DIB_HEADER_SIZE = 40;
 static const unsigned char BM_MAGIC[2] = {0x42, 0x4D};
 static const uint32_t DIB_HEADER_VAL = DIB_HEADER_SIZE;
 static const uint32_t HEADERS_SIZE = BM_HEADER_SIZE + DIB_HEADER_SIZE;
+static const size_t DWORD = sizeof(uint32_t);
+static const size_t WORD  = sizeof(uint16_t);
+
+static int32_t bw_pixel_width(uint32_t dataLen);
+static int32_t bw_pixel_height(uint32_t dataLen);
+
+static void memcpy_uint(unsigned char * loc, uint32_t value);
+static void memcpy_int(unsigned char * loc, int32_t value);
 
 bmp_result * create_bw_bmp(const unsigned char * data, const uint32_t dataLen) {
     bmp_result * const result = (bmp_result *)calloc(1, sizeof(bmp_result));
-    result->dataSize = HEADERS_SIZE;
-    result->data = (unsigned char *)calloc(HEADERS_SIZE, sizeof(unsigned char));
 
-    memcpy(result->data, BM_MAGIC, sizeof(BM_MAGIC));
-    memcpy(result->data + BM_HEADER_SIZE, &DIB_HEADER_VAL, sizeof(uint32_t));
+    unsigned char * bmpHeader = 
+        (unsigned char *)calloc(HEADERS_SIZE, sizeof(unsigned char));
+    result->data = bmpHeader;
+    result->dataSize = HEADERS_SIZE;
+    
+    memcpy(bmpHeader, BM_MAGIC, sizeof(BM_MAGIC));
+
+    unsigned char * dibHeader = bmpHeader + BM_HEADER_SIZE;
+  
+    memcpy_uint(dibHeader, DIB_HEADER_SIZE);
+    dibHeader += DWORD;
+    memcpy_int(dibHeader, bw_pixel_width(dataLen));
+    dibHeader += DWORD;
+    memcpy_int(dibHeader, bw_pixel_height(dataLen));
+  
     return result;
 }
-
 
 void bmp_free(bmp_result * result) {
     result->dataSize = 0;
     free(result->data);
     free(result); 
 }
+
+static int32_t bw_pixel_width(uint32_t dataLen) {
+    return dataLen * CHAR_BIT;
+}
+
+static int32_t bw_pixel_height(uint32_t dataLen) {
+    return 1;
+}
+
+static void memcpy_uint(unsigned char * loc, uint32_t value) {
+    *((uint32_t *)loc) = value;
+}
+
+static void memcpy_int(unsigned char * loc, int32_t value) {
+    *((int32_t *)loc) = value;
+}
+
--- test/bmpLibTest.c
+++ test/bmpLibTest.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdint.h>
+#include <limits.h>
 
 #include <check.h>
 
@@ -9,18 +10,26 @@
 
 // Constants
 
+static const size_t WORD = sizeof(uint16_t);
+static const size_t DWORD = sizeof(uint32_t);
 static const uint32_t BM_HEADER_SIZE = 14;
 static const uint32_t DIB_HEADER_SIZE = 40;
+static const uint32_t DIB_HEADER_VAL = DIB_HEADER_SIZE;
 static const unsigned char BM_MAGIC[2] = {0x42, 0x4D}; 
-
 // Binary for 10101010
 // so black, white, black white etc.
 static const unsigned char TEST_INPUT = 0xAA;
+static const uint32_t TEST_SIZE = sizeof(unsigned char);
+static const uint32_t TEST_BIT_SIZE = TEST_SIZE * CHAR_BIT;
+static const int32_t EXPECT_WIDTH = TEST_BIT_SIZE;
+static const int32_t EXPECT_HEIGHT = 1;
+
+
 static bmp_result * result;
 
 // Setup / teardown
 void setup(void) {
-    result = create_bw_bmp(&TEST_INPUT, 1);
+    result = create_bw_bmp(&TEST_INPUT, TEST_SIZE);
 }
 
 void teardown(void) {
@@ -30,6 +39,8 @@ void teardown(void) {
 }
 
 // Custom assert decls
+
+
 static void assert_struct_nerr(const bmp_result * const bmp);
 
 //Tests go here
@@ -43,17 +54,34 @@ START_TEST(test_bmp_header_valid)
 {
     const int32_t size = result->dataSize;
     const unsigned char * data = result->data;
-    ck_assert_msg(size >= BM_HEADER_SIZE, "Size must be greater than BM_HEADER_SIZE");
-    ck_assert_msg(memcmp(BM_MAGIC, data, sizeof(BM_MAGIC)) ==  0, "BMP magic number not found");
+
+    ck_assert_msg(size >= BM_HEADER_SIZE, 
+        "Size must be greater than BM_HEADER_SIZE");
+    ck_assert_msg(memcmp(BM_MAGIC, data, sizeof(BM_MAGIC)) ==  0,
+        "BMP magic number not found");
 }
 END_TEST
 
 START_TEST(test_dib_header_valid) 
 {
     const int32_t size = result->dataSize;
-    ck_assert_msg(size >= BM_HEADER_SIZE + DIB_HEADER_SIZE, "Size must be greater than BM_HEADER_SIZE + DIB_HEADER_SIZE (BMP header + DIB Header)");
+    ck_assert_msg(size >= BM_HEADER_SIZE + DIB_HEADER_SIZE, 
+        "Size must be greater than BM_HEADER_SIZE + DIB_HEADER_SIZE (BMP header + DIB Header)");
+
     const unsigned char * dibData = result->data + BM_HEADER_SIZE;
-    ck_assert_msg(memcmp(&DIB_HEADER_SIZE, dibData, sizeof(uint32_t)) ==  0, "Expecting header size to be present as first entry in dib header");
+    ck_assert_msg(memcmp(&DIB_HEADER_VAL, dibData, DWORD) == 0, 
+        "Expecting header size to be present as first entry in dib header");
+
+    dibData += DWORD;
+
+    ck_assert_msg(memcmp(&EXPECT_WIDTH, dibData, DWORD) == 0, 
+        "Expected width to equal ");
+
+    dibData += DWORD;
+
+    ck_assert_msg(memcmp(&EXPECT_HEIGHT, dibData, DWORD) == 0, 
+        "Expected height to equal ");
+
 }
 END_TEST
 
